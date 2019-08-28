a84a71cd9331cd5a23ac41652c9606c52efe708e
조원희 <wonhee@jowonhuiui-MacBookPro.local>
2019-08-29 06:46:22 +0900

    changed some static settings in settings.py

--- final1/settings.py
+++ final1/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_DIR =(
+    os.path.join(PROJECT_ROOT, 'static')
+)
 
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static')
-MEDIA_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+MEDIA_URL = '/media/'
