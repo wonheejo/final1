1998f7969f877c1cf8f47ab78fbd25e0dfcc487f
조원희 <wonhee@jowonhuiui-MacBookPro.local>
2019-08-28 13:26:37 +0900

    second commit

--- final1/settings.py
+++ final1/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+MEDIA_URL = '/final1/'
+MEDIA_ROOT = '/usr/wonhee/final1/'
