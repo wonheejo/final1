dab3e324e112e309f3147220b9cac2fc84f3ded8
조원희 <wonhee@jowonhuiui-MacBookPro.local>
2019-08-29 07:00:45 +0900

    got rid of annoying dir paths in settings.py

--- final1/settings.py
+++ final1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'final1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '/final1/templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- final1/urls.py
+++ final1/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.button),
     path('output', views.output, name='script')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
