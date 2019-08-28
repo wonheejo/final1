692d2915f66b40fb72bcaadc81b32015b0db3e40
조원희 <wonhee@jowonhuiui-MacBookPro.local>
2019-08-29 06:29:37 +0900

    added static component in urls.py

--- final1/urls.py
+++ final1/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.button),
     path('output', views.output, name='script')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
